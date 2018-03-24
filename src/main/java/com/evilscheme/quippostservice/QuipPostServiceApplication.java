ef57c1aef8863db94f27d4bc22af3eb489c02185
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-24 16:18:50 -0400

    Project init.

---
+++ src/main/java/com/evilscheme/quippostservice/QuipPostServiceApplication.java
@@ -0,0 +1,12 @@
+package com.evilscheme.quippostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipPostServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipPostServiceApplication.class, args);
+	}
+}
