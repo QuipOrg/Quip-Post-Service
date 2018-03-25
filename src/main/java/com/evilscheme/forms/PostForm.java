14263a5c454f80e941a846583392e4fa3750ecee
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-25 09:43:20 -0400

    Add post form.

---
+++ src/main/java/com/evilscheme/forms/PostForm.java
@@ -0,0 +1,31 @@
+package com.evilscheme.forms;
+
+public class PostForm {
+    private String title;
+    private String description;
+    private String media;
+
+    public PostForm() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+    public void setMedia(String media) {
+        this.media = media;
+    }
+}
